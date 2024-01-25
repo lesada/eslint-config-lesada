46e1fd6e42b476d45f76a7bc0fc7dd5ebe389071
Lauren Leslie Ferreira <laurenlesliesilva@gmail.com>
2024-01-25 15:52:27 -0300

    feat: react main rules

---
+++ index.js
@@ -0,0 +1,55 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "prettier",
+  ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  plugins: ["react", "eslint-plugin-import-helpers", "unused-imports"],
+  rules: {
+    "import-helpers/order-imports": [
+      "warn",
+      {
+        newlinesBetween: "always",
+        groups: [
+          "/^react$/",
+          "/^next$/",
+          "module",
+          "/^@//",
+          ["sibling", "index"],
+          "parent",
+          "/./styles/",
+        ],
+        alphabetize: { order: "asc", ignoreCase: true },
+      },
+    ],
+    "unused-imports/no-unused-imports": "error",
+
+    "unused-imports/no-unused-vars": [
+      "warn",
+      {
+        vars: "all",
+        varsIgnorePattern: "^_",
+        args: "after-used",
+        argsIgnorePattern: "^_",
+      },
+    ],
+    "@typescript-eslint/no-explicit-any": "off",
+  },
+};
