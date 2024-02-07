2b7872cc166bdb3cc1719caf8f53014bb2b1e92c
Lauren Leslie Ferreira <laurenlesliesilva@gmail.com>
2024-02-07 10:41:08 -0300

    fix: dependencies

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ module.exports = {
         newlinesBetween: "always",
         groups: [
           "/^react$/",
+          "/^react-native$/",
           "/^next$/",
           "module",
           "/^@//",
